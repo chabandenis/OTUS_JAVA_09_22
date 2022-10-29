ea97048b73a183434e8e80866c22e47538ba68ee
Denis <denisbrd8@gmail.com>
2022-10-29 14:46:43 +0700

    1

--- L04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ L04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,16 +1,20 @@
 package homework;
 
 
+import java.util.*;
+
 public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    TreeSet lst = new TreeSet();
 
     public void add(Customer customer) {
-
+        lst.add(customer);
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+
+       return null; // это "заглушка, чтобы скомилировать"
     }
 }
--- L04-generics/src/main/java/homework/CustomerService.java
+++ L04-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,65 @@
 package homework;
 
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class CustomerService {
+
+    Map<Customer, String> lst = new HashMap<>();
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> forRet = null;
+
+        for (Map.Entry entry : lst.entrySet()) {
+            Customer cst = (Customer) entry.getKey();
+            if (forRet == null) {
+                forRet = entry;
+            } else {
+                if (cst.getScores() < forRet.getKey().getScores()) {
+                    forRet = entry;
+                }
+            }
+        }
+
+        return forRet; // это "заглушка, чтобы скомилировать"
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> forRet = null;
+
+        System.out.println(
+                lst.entrySet()
+                        .stream()
+                        .filter(e -> e.getKey().getScores() > customer.getScores())
+                        .collect(Collectors.toList())
+        );
+
+        for (Map.Entry entry : lst.entrySet()
+                .stream()
+                .filter(e -> e.getKey().getScores() > customer.getScores())
+                .collect(Collectors.toList())
+        ) {
+            Customer cst = (Customer) entry.getKey();
+            if (forRet == null) {
+                forRet = entry;
+            } else {
+                if (cst.getScores() < forRet.getKey().getScores()) {
+                    forRet = entry;
+                }
+            }
+        }
+
+        return forRet; // это "заглушка, чтобы скомилировать"
     }
 
     public void add(Customer customer, String data) {
-
+        lst.put(customer, data);
     }
 }
