b5be4f853dc78edd989e2c22254b75a785ebfa4d
Denis <denisbrd8@gmail.com>
2023-03-01 16:36:28 +0700

    log

--- Log_new/src/main/java/ru/denis_ch/domain/TestLogging.java
+++ Log_new/src/main/java/ru/denis_ch/domain/TestLogging.java
@@ -1,21 +1,14 @@
 package ru.denis_ch.domain;
 
-import ru.denis_ch.service.Log;
-
 interface TestLogging {
 
-    @Log
     void calculation1(int param001);
-
 
     void calculation2(int param002);
 
-    @Log
     void calculation3(int param003);
-
 
     void calculation4(int param004);
 
-    @Log
     void calculation5(int param005);
 }
